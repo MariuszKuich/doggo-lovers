324000eee31ab4c7e40850d0aeb10b8fb19a23c3
MariuszKuich <mariuszkuich@outlook.com>
2022-06-21 01:15:33 +0200

    signing in improvement - encrypting passwords in the database

--- src/main/java/pl/mariuszk/security/SecurityConfiguration.java
+++ src/main/java/pl/mariuszk/security/SecurityConfiguration.java
@@ -1,15 +1,24 @@
 package pl.mariuszk.security;
 
 import com.vaadin.flow.spring.security.VaadinWebSecurityConfigurerAdapter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import pl.mariuszk.viewcontrollers.LoginView;
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfiguration extends VaadinWebSecurityConfigurerAdapter {
+
+	private final CustomUserDetailsService customUserDetailsService;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
@@ -23,5 +32,23 @@ public class SecurityConfiguration extends VaadinWebSecurityConfigurerAdapter {
 				"/images/**"
 		);
 		super.configure(web);
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(authProvider());
+	}
+
+	@Bean
+	public PasswordEncoder encoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public DaoAuthenticationProvider authProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(customUserDetailsService);
+		authProvider.setPasswordEncoder(encoder());
+		return authProvider;
 	}
 }
---
+++ src/main/resources/sql/users_sample_insert.sql
@@ -0,0 +1,7 @@
+INSERT INTO USERS (USERNAME, PASSWORD) VALUES
+    --pwd zaq1@WSX
+    ('ala18', '$2a$10$I6JrKnVwjBDa5daFI3H7fulhEmHinpQeyyxOcOXFhhuGUNblce/Ha'),
+    --pwd 123qwe
+    ('oliwier_koc', '$2a$10$zf4fdiHba/u4.3epYIupS.Ux9Z8t7mz9OTRtDOQqECrLOsb7uxwDO'),
+    --pwd XSW@3edc
+    ('bajgiel3', '$2a$10$tTI2RwZv9feoOaTlZPLfle7s/q3sC4JE19P3yNRmfwJXWjDXSJ1ue');
