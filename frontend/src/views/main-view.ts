6954d8c42436c728382156606b5ff43cfaa1021d
MariuszKuich <mariuszkuich@outlook.com>
2022-06-22 09:05:14 +0200

    prepare main view draft

--- frontend/src/views/main-view.ts
+++ frontend/src/views/main-view.ts
@@ -1,7 +1,8 @@
 import { LitElement, html, css, customElement } from 'lit-element';
 import '@vaadin/vertical-layout/src/vaadin-vertical-layout.js';
-import '@vaadin/text-field/src/vaadin-text-field.js';
+import '@vaadin/horizontal-layout/src/vaadin-horizontal-layout.js';
 import '@vaadin/button/src/vaadin-button.js';
+import '@polymer/iron-icon/iron-icon.js';
 
 @customElement('main-view')
 export class MainView extends LitElement {
@@ -17,11 +18,27 @@ export class MainView extends LitElement {
   render() {
     return html`
 <vaadin-vertical-layout style="width: 100%; height: 100%; align-items: center;">
- <vaadin-text-field label="Username" id="lblUsername" type="text">
-  drdr
- </vaadin-text-field>
- <vaadin-button theme="primary" id="btnLogout" tabindex="0">
-  Logout
+ <h1 id="hWelcome">Hello, {placeholder}!</h1>
+ <vaadin-vertical-layout id="btnDogData" style="align-items: center;" theme="spacing-xs">
+  <vaadin-horizontal-layout theme="spacing-s">
+   <vaadin-button id="vaadinButton" style="padding: var(--lumo-space-l);" tabindex="0" theme="secondary">
+    <iron-icon icon="lumo:plus" slot="prefix"></iron-icon>Add/Edit Dog Data 
+   </vaadin-button>
+   <vaadin-button style="padding: var(--lumo-space-l);" tabindex="0" id="btnScheduler" theme="secondary success">
+    <iron-icon icon="lumo:calendar" slot="prefix"></iron-icon>Schedule Walking The Dog
+   </vaadin-button>
+  </vaadin-horizontal-layout>
+  <vaadin-horizontal-layout theme="spacing-s">
+   <vaadin-button style="padding: var(--lumo-space-l);" tabindex="0" id="btnGallery" theme="secondary error">
+    <iron-icon icon="lumo:eye" slot="prefix"></iron-icon>Dogs Gallery
+   </vaadin-button>
+   <vaadin-button style="padding: var(--lumo-space-l);" tabindex="0" id="btnVet" theme="secondary contrast">
+    <iron-icon icon="lumo:search" slot="prefix"></iron-icon>History Of Visits To The Vet
+   </vaadin-button>
+  </vaadin-horizontal-layout>
+ </vaadin-vertical-layout>
+ <vaadin-button theme="primary" id="btnLogout" tabindex="0" style="margin: var(--lumo-space-xl);">
+   Logout 
  </vaadin-button>
 </vaadin-vertical-layout>
 `;
--- src/main/java/pl/mariuszk/viewcontroller/MainView.java
+++ src/main/java/pl/mariuszk/viewcontroller/MainView.java
@@ -3,9 +3,9 @@ package pl.mariuszk.viewcontroller;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.JsModule;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.littemplate.LitTemplate;
 import com.vaadin.flow.component.template.Id;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -23,12 +23,20 @@ import java.util.Optional;
 @JsModule("./src/views/main-view.ts")
 public class MainView extends LitTemplate {
 
-	@Id("lblUsername")
-	private TextField lblUsername;
+	@Id("hWelcome")
+	private H1 hWelcome;
 	@Id("btnLogout")
 	private Button btnLogout;
+	@Id("vaadinButton")
+	private Button vaadinButton;
+	@Id("btnScheduler")
+	private Button btnScheduler;
+	@Id("btnGallery")
+	private Button btnGallery;
+	@Id("btnVet")
+	private Button btnVet;
 
-    public MainView(SecurityService securityService, UserService userService) {
+	public MainView(SecurityService securityService, UserService userService) {
 		Optional<UserDetails> loggedUser = Optional.ofNullable(securityService.getAuthenticatedUser());
 
 		loggedUser.ifPresent(lu -> {
@@ -36,7 +44,7 @@ public class MainView extends LitTemplate {
 				userService.registerNewOAuthUserIfNecessary((CustomOAuthUserPrincipal) lu);
 			}
 		});
-		loggedUser.ifPresent(lu -> lblUsername.setValue(lu.getUsername()));
+		loggedUser.ifPresent(lu -> hWelcome.setText("Welcome, " + lu.getUsername() + "!"));
 
 		btnLogout.addClickListener(e -> loggedUser.ifPresent(lu -> securityService.logout()));
     }
